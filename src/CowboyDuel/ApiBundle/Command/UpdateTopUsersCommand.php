3e2508ef5d04a33f6350d3f5a79319b5fdc245b2
Kordyuk Jura <Jura@webkate.com>
2012-11-15 13:19:36 +0200

    add console command for update list top users

--- src/CowboyDuel/ApiBundle/Command/UpdateTopUsersCommand.php
+++ src/CowboyDuel/ApiBundle/Command/UpdateTopUsersCommand.php
@@ -12,58 +12,21 @@ class UpdateTopUsersCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName('update:store')
-            ->setDescription('Update file on S3 top users')
-            ->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?')
+            ->setName('update:ListTopUsers')
+            ->setDescription('Update file on S3: List Top Users')
             ->addOption('time', null, InputOption::VALUE_NONE, 'Show time update')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $name = $input->getArgument('name');
+    {        
+        $listTopUsers = $this->getContainer()->get('CowboyDuel.UpdateListTopUsers'); 
+        $listTopUsers->update();
         
-        
-
+        $text = '';
         if ($input->getOption('time')) 
-        {
-            $text .= ' '.$time;
-        }
+        	$text .= ' Updating time: '.time();        
 
         $output->writeln($text);
     }
 }
-/*
- namespace Acme\DemoBundle\Controller; 
- use Symfony\Component\DependencyInjection\ContainerInterface; // Сервис для управления состоянием задач
- 
- class State 
- { 
- 	private $container; 
- 	public function __construct(ContainerInterface $container) 
- 	{
- 
-		$this->container = $container;
-	} 
-public function update()
-{
-	$em = $this->container ->get('doctrine') ->getEntityManager(); 
-	$currentDate = new \DateTime('now'); //запрос на все задачи 
-	$query = $em->createQueryBuilder() 
-	->select('t') 
-	->from('AcmeDemoBundle:Task', 't') 
-	->getQuery() ; 
-	$tasks = $query->getResult();
-	 if($tasks) { foreach($tasks as $task) 
-	 { 
-	 	$diff = $task->getPlan()->getTimestamp() - $currentDate->getTimestamp(); 
-	 	if($diff > 0 && (($diff / 86400) > 1)) 
-	 	{ //если просрочена планируемая дата, то ставим статус Не выполнено 
-	 		$state = $em->getRepository('AcmeDemoBundle:State')->findOneByName('Не выполнено'); 
-	 		if($state) { $task->setState($state); } } 
-	 		else { $task->setState(null); } } 
-	 		$em->flush(); 
-	 } 
-} 
-}
-	 }*/
---
+++ src/CowboyDuel/ApiBundle/DependencyInjection/UpdateListTopUsers.php
@@ -0,0 +1,38 @@
+<?php
+namespace CowboyDuel\ApiBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection,
+	Doctrine\ORM\EntityManager;
+
+use CowboyDuel\ApiBundle\Helper\HelperQueryHolds,
+	CowboyDuel\ApiBundle\Helper\HelperMethod;
+
+class UpdateListTopUsers
+{
+	protected $em;
+	protected $container;
+	
+	public function __construct($_container, EntityManager $entityManager)
+	{
+		$this->em = $entityManager;
+		$this->container = $_container;
+	}
+	
+    public function update()
+    {
+    	$queryHolds = new HelperQueryHolds($this->em);
+    	
+    	$entitiesJson = json_encode($queryHolds->get_top_users());
+    		 
+    	$helperMethod = new HelperMethod($this->container);
+    	$helperMethod->sendStatS3("web/".$this->container->getParameter('S3_topBoard_file_upload'),
+    							  $this->container->getParameter('S3_topBoard_uri')
+    							  .$this->container->getParameter('S3_type_file'),
+    							  $entitiesJson);  		
+    	$queryHolds->setSettings('timeLastRefresh',time());    
+    	
+    	$queryHolds = new HelperQueryHolds($this->em);
+    	 
+    	$entitiesJson = json_encode($queryHolds->get_top_users());
+    }
+}
