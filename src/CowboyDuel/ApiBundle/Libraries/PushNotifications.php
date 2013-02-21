fe349f71b9662251d7a3560e7a8e639763f10745
Kordyuk Jura <jura@webkate.com>
2013-02-21 15:29:21 +0200

    change pem

--- src/CowboyDuel/ApiBundle/Libraries/PushNotifications.php
+++ src/CowboyDuel/ApiBundle/Libraries/PushNotifications.php
@@ -6,7 +6,7 @@ class PushNotifications
 		private $apnsHost = 'gateway.sandbox.push.apple.com';
 		//private $apnsHost = 'gateway.push.apple.com';
 		private $apnsPort = '2195';
-		private $sslPem = 'apns-dev.pem';
+		private $sslPem = 'apns.pem';
 		private $passPhrase = '1111';
 		
 		private $apnsConnection;
